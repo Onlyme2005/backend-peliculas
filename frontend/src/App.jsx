9efb6245e3adcd79cfa99ee29dbf2e0625116248
Onlyme2005 <brislleily.carmona@est.iudigital.edu.co>
2026-03-29 13:21:52 -0500

    Eliminar mensaje de bienvenida del panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,6 @@ import { DirectorView } from './pages/DirectorView';
 import { ProductoraView } from './pages/ProductoraView';
 import { MediaView } from './pages/MediaView';
 
-// Vistas temporales (Más adelante crearemos archivos reales para estas)
-const Inicio = () => <div className="container mt-4"><h1>Bienvenido al Panel de Administración</h1><p>Selecciona un módulo en el menú superior.</p></div>;
-
 
 function App() {
   return (
@@ -18,7 +15,7 @@ function App() {
       
       {/* Aquí cambian las pantallas mágicamente */}
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<MediaView />} />
         <Route path="/generos" element={<GeneroView />} />
         <Route path="/directores" element={<DirectorView />} />
         <Route path="/productoras" element={<ProductoraView />} />
